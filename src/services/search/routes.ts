f489d0feb6a66c8adf58ab1bc1c629a9c5ddbc08
godspowerosilama <osilamagodspower@gmail.com>
2019-08-25 21:29:51 +0100

    Basic error handling Code clean up Update package.json

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,11 +1,11 @@
-import { Request, Response} from "express";
+import { Request, Response} from 'express';
 
 export default [
     {
         path: "/",
         method: "get",
         handler: async (req: Request, res: Response) => {
-            res.send("Hello World!");
+            res.send('Hello World!');
         }
     }
 ]
