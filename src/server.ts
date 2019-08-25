6979865def492f636828685899ef0a36056c8017
godspowerosilama <osilamagodspower@gmail.com>
2019-08-25 21:04:34 +0100

    Add base route Basic setup for error handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import http from 'http';
 import express from 'express';
-import { applyMiddleware } from './lib';
+import { applyMiddleware, applyRoutes } from './lib';
 import middleware from './middleware';
+import routes from './services';
+
+process.on('uncaughtException', e => {
+    console.log(e);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', e => {
+    console.log(e);
+    process.exit(1);
+});
 
 const router = express();
 applyMiddleware(middleware, router);
+applyRoutes(routes, router);
 
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
